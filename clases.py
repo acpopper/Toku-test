e8442289ae58b34d7e49a90fe37717ada1f964c1
acpopper <acpopper@uc.cl>
2022-03-09 02:54:36 -0300

    armado de equipos

--- clases.py
+++ clases.py
@@ -5,6 +5,10 @@ class Character:
     def __init__(self, id, name, stats_dict, al):
         self.id = id
         self.name = name
+        # hay muchos superheroes con stats null, se reemplazarán con 0
+        for k in stats_dict:
+            if stats_dict[k] == 'null':
+                stats_dict[k] = 0
         # "base modificada de cada stat", luego se aplicará el bonus de equipo
         self.intelligence = (2*int(stats_dict['intelligence']) + random.randint(0,10))/1.1
         self.strength = (2*int(stats_dict['strength']) + random.randint(0,10))/1.1
@@ -14,23 +18,17 @@ class Character:
         self.combat = (2*int(stats_dict['combat']) + random.randint(0,10))/1.1
         # data auxiliar
         self.alignment = al
-        self._FB = 0
         self.hp_mod_AS = (1+random.randint(0,10)/10)
     
-    @property
-    def hp(self):
-        return self._hp
-
-    @hp.setter
-    def hp(self, value):
-        pass
-    
     # Se considera que se usan los stats "reales" para calcular el hp
+    # Hay heroes 'neutral', no se les aplicará ni buff ni debuff
     def apply_team_bonus(self, team_alignment):
         if team_alignment == self.alignment:
-            self.FB =  1+random.randint(0, 9)
+            self.FB = 1+random.randint(0, 9)
+        elif self.alignment == 'neutral':
+            self.FB = 1
         else:
-            self.FB =  (1+random.randint(0, 9))^-1
+            self.FB = (1+random.randint(0, 9))**(-1)
         
         self.intelligence = floor(self.intelligence*self.FB)
         self.strength = floor(self.strength*self.FB)
@@ -38,18 +36,43 @@ class Character:
         self.durability = floor(self.durability*self.FB)
         self.power = floor(self.power*self.FB)
         self.combat = floor(self.combat*self.FB)
-        self._hp = floor((self.strength*0.8 + self.durability*0.7 + self.power)*self.hp_mod_AS/2) + 100
+        self.hp = floor((self.strength*0.8 + self.durability*0.7 + self.power)*self.hp_mod_AS/2) + 100
+        self.full_hp = self.hp
 
     def atacar(self):
         ataque = random.choice([self.mental, self.strong, self.fast])
-        return ataque
+        return ataque()
 
     def mental(self):
-        return (self.intelligence*0.7 + self.speed*0.2+ self.combat*0.1)*self.FB
+        daño = (self.intelligence*0.7 + self.speed*0.2+ self.combat*0.1)*self.FB
+        print(f"{self.name} ha usado ataque mental!")
+        print(f"Inflige {daño} de daño!")
+        return daño
 
     def strong(self):
-        return (self.strength*0.6 + self.power*0.2+ self.combat*0.2)*self.FB
+        daño = (self.strength*0.6 + self.power*0.2+ self.combat*0.2)*self.FB
+        print(f"{self.name} ha usado ataque strong!")
+        print(f"Inflige {daño} de daño!")
+        return daño
 
     def fast(self):
-        return (self.speed*0.55 + self.durability*0.25+ self.strength*0.2)*self.FB
+        daño = (self.speed*0.55 + self.durability*0.25+ self.strength*0.2)*self.FB
+        print(f"{self.name} ha usado ataque fast!")
+        print(f"Inflige {daño} de daño!")
+        return daño
 
+    def reset_health(self):
+        self.hp = self.full_hp
+
+    def __repr__(self):
+        return f'{self.name} [{self.hp}/{self.full_hp}]'
+
+if __name__ == "__main__":
+    a = ['Professor Zoom'
+    ,{'intelligence': '94', 'strength': '10', 'speed': '100', 'durability': '20', 'power': '83', 'combat': '20'}
+    ,'bad']
+
+    c = Character(1, a[0], a[1], a[2])
+    c.apply_team_bonus('bad')
+    print(c)
+    c.atacar()
--- main.py
+++ main.py
@@ -2,12 +2,53 @@ import requests
 from clases import Character
 from data import API_KEY
 import random
+from math import floor
 
-res = requests.get(f"https://superheroapi.com/api/{API_KEY}/{random.randint(1, 731)}")
+num_superheroes = 731
+# res = requests.get(f"https://superheroapi.com/api/{API_KEY}/{random.randint(1, num_superheroes)}")
+# print(res.json()['id'])
+# print(res.json()['name'])
+# print(res.json()['powerstats'])
+# print(res.json()['biography']['alignment'])
 
+def simulation():
+    team_a = {'team': [], 'alignment': 'bad'}
+    team_b = {'team': [], 'alignment': 'bad'}
+    drafts = set()
+    # seleccionamos id's que no se repitan
+    while len(drafts) < 10:
+        drafts.add(random.randint(1, num_superheroes))
+    
+    # poblamos los equipos
+    team_a_al = 0
+    team_b_al = 0
+    for i, hero in enumerate(drafts):
+        personaje = requests.get(f"https://superheroapi.com/api/{API_KEY}/{hero}").json()
+        if i < len(drafts)/2:
+            team_a['team'].append(Character(personaje['id'], personaje['name'], personaje['powerstats'], personaje['biography']['alignment']))
+            if personaje['biography']['alignment'] == 'good':
+                team_a_al += 1
+                if team_a_al == 3:
+                    team_a['alignment'] = 'good'
+
+        else:
+            team_b['team'].append(Character(personaje['id'], personaje['name'], personaje['powerstats'], personaje['biography']['alignment']))
+            if personaje['biography']['alignment'] == 'good':
+                team_b_al += 1
+                if team_b_al == 3:
+                    team_b['alignment'] = 'good'
+
+    for i in team_a['team']:
+        i.apply_team_bonus(team_a['alignment'])
+    for i in team_b['team']:
+        i.apply_team_bonus(team_b['alignment'])
+    print('Team A')
+    for i in team_a['team']:
+        print(i)
+    print('Team B')
+    for i in team_b['team']:
+        print(i)
 
 
 if __name__ == "__main__":
-    print(res.json()['name'])
-    print(res.json()['powerstats'])
-    print(res.json()['biography']['alignment'])
+    simulation()
