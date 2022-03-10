de0be46bace019e1cd02ad37bf04fef31fc66f0b
acpopper <acpopper@uc.cl>
2022-03-10 20:27:21 -0300

    ready, bit of format missing

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from time import sleep
-from pyrsistent import T
 import requests
 from clases import Character
 from data import API_KEY, mailgun_api
 import random
-from math import floor
 
+# Total de superheroes de la API
 num_superheroes = 731
-# res = requests.get(f"https://superheroapi.com/api/{API_KEY}/{random.randint(1, num_superheroes)}")
-# print(res.json()['id'])
-# print(res.json()['name'])
-# print(res.json()['powerstats'])
-# print(res.json()['biography']['alignment'])
 
 def build_teams():
     team_a = {'team': [], 'alignment': 'bad'}
@@ -54,60 +48,68 @@ def simulacion(team_a, team_b, email=None):
     pb = 0
     player_a = team_a['team'][pa]
     player_b = team_b['team'][pb]
-
+    results = ''
     print(' '*10 + '-- INICIA LA PELEA!! --')
     print('Equipo A' + ' '*10 + 'vs' + ' '*10 + 'Equipo B')
+    results += 'Equipo A' + ' '*10 + 'vs' + ' '*10 + 'Equipo B\n'
     for i in range(0, 5):
         print(f"{team_a['team'][i]} -- {team_b['team'][i]}")
-    print('\n \n')
+        results += f"{team_a['team'][i]} -- {team_b['team'][i]}\n"
+    results += '\n'
+    print('\n')
 
     ronda = 1
-    turno = 1
-    results = 'Resultados peleas\n'
     while not team_a['defeated'] or not team_b['defeated']:
-        print(f'Ronda {ronda}--Turno {turno}')
+        sleep(3)
+        print(f'**Ronda {ronda}: {player_a.name} vs {player_b.name}**')
         player_b.recibir_daño(player_a.name, player_a.atacar())
         if player_b.hp == 0:
-            print(f"{player_a.name} ha derrotado a {player_b.name}")
-            results + f'Ronda {ronda}: {player_a.name} (winner) vs {player_b.name}\n'
+            print(f"{player_a.name} ha derrotado a {player_b.name}!!\n-----")
+            results += f'Ronda {ronda}: {player_a.name} (winner) vs {player_b.name}\n'
             ronda += 1
             pb += 1
             if pb < 5:
                 player_b = team_b['team'][pb]
                 player_a.reset_health()
+                continue
             else:
                 team_b['defeated'] = True
+                break
         
-        sleep(1)
+        sleep(3)
         player_a.recibir_daño(player_b.name, player_b.atacar())
         if player_a.hp == 0:
-            print(f"{player_b.name} ha derrotado a {player_a.name}")
-            results + f'Ronda {ronda}: {player_a.name} vs {player_b.name} (winner)\n'
+            print(f"{player_b.name} ha derrotado a {player_a.name}!!\n-----")
+            results += f'Ronda {ronda}: {player_a.name} vs {player_b.name} (winner)\n'
             ronda += 1
             pa += 1
             if pa < 5:
                 player_a = team_a['team'][pa]
                 player_b.reset_health()
+                continue
             else:
                 team_a['defeated'] = True
-        
-        sleep(1)
-        
+                break
             
-
     
-    pass
+    print('--- LAS PELEAS HAN FINALIZADO! ---')
+    results += '--- LAS PELEAS HAN FINALIZADO! ---\n'
+    print(f"Equipo ganador: {'Equipo A!' if team_b['defeated'] else 'Equipo B!'}")
+    results += "Equipo ganador: \033[1m" + f"{'Equipo A!' if team_b['defeated'] else 'Equipo B!'}" + "\033[0m \n"
 
-def send_simple_message(to):
+    if email:
+        send_simple_message(email, results)
+
+def send_simple_message(to, text):
 	return requests.post(
 		"https://api.mailgun.net/v3/sandbox62fd7184edf44a54ad96e60f670bc76d.mailgun.org/messages",
 		auth=("api", mailgun_api),
 		data={"from": "Alan Popper <acpopper@uc.cl>",
 			"to": to,
 			"subject": "Resultado simulación",
-			"text": ""})
+			"text": text})
 
 
 if __name__ == "__main__":
     A, B = build_teams()
-    simulacion(A, B)
+    simulacion(A, B, 'acpopper@uc.cl')  # Añadir en un tercer parámetro el email si se quiere enviar
